5a4a2c921abe394e2df981840f1ad361646f362c
ZYY <2893356240@qq.com>
2020-12-27 21:45:27 +0800

    初始化工程

---
+++ PictureServer/src/main/java/com/yan/PictureServerApplication.java
@@ -0,0 +1,13 @@
+package com.yan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureServerApplication.class, args);
+    }
+
+}
